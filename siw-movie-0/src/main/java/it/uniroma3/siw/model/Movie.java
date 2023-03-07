903ea87d14bd2b8b0baed38cee97a580a1227bff
Corrado Munafò <82174687+dinho99@users.noreply.github.com>
2023-03-07 18:47:01 +0100

    Added default class

---
+++ siw-movie-0/src/main/java/it/uniroma3/siw/model/Movie.java
@@ -0,0 +1,54 @@
+package it.uniroma3.siw.model;
+
+public class Movie {
+
+    private Long id;
+    private String title;
+    public Integer year;
+    public String urlImage;
+
+    /* getter and setter methods */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getYear() {
+        return this.year;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getUrlImage() {
+        return this.urlImage;
+    }
+
+    /* equals and hashCode */
+    @Override
+    public boolean equals (Object o) {
+        final Movie that = (Movie) o;
+        return ( this.getTitle().equals(that.getTitle()) && this.getYear() == that.getYear() );
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getTitle().hashCode() + this.getYear().hashCode();
+    }
+}
